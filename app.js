2b31b3b717f37401b9525ac8570feb6f23770f55
eranash <45369755+eranash@users.noreply.github.com>
2021-10-04 15:55:19 +0300

    v1.1

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
-const grid = document.querySelector(".grid")
+const grid = document.querySelector(".grid");
 var gBoard;
-
 
 var size = 4;
 var bombAmount = 2;
@@ -11,35 +10,49 @@ const bombs = Array(bombAmount).fill(BOMB);
 const valids = Array(size * size - bombAmount).fill(VALID);
 const gCells = bombs.concat(valids);
 // console.log(gCells)
-createBoard(size)
+createBoard(size);
 console.log(gBoard);
-
 
 function createBoard() {
   const board = [];
   for (var i = 0; i < size; i++) {
     board.push([]);
-    for (var j = 0;  j< size; j++) {
-      board[i][j] = drawNum()
+    for (var j = 0; j < size; j++) {
+      setMinesNegsCount(i, j, board);
+      board[i][j] = drawNum();
     }
   }
-  gBoard = board
-  
+  gBoard = board;
 }
 
-renderBoard(gBoard)
-function renderBoard (){
-    var idx=0;
-    for (var i=0; i<size; i++){
-        var elRow = document.createElement('tr'); 
-        grid.appendChild(elRow);
-        for (var j=0; j<size; j++){
-            var elCell = document.createElement('td');
-            elRow.appendChild(elCell);
-            elCell.setAttribute('id',idx);
-            elCell.classList.add(gBoard[i][j].cellType)
-            idx++
-        }
-
+renderBoard(gBoard);
+function renderBoard() {
+  var idx = 0;
+  for (var i = 0; i < size; i++) {
+    var elRow = document.createElement("tr");
+    grid.appendChild(elRow);
+    for (var j = 0; j < size; j++) {
+      var elCell = document.createElement("td");
+      elRow.appendChild(elCell);
+      elCell.setAttribute("id", idx);
+      elCell.classList.add(gBoard[i][j].cellType);
+      idx++;
+      setMinesNegsCount(i, j, gBoard);
+      elCell.setAttribute("data", minesNegsCount);
     }
+  }
+}
+
+
+var minesNegsCount = 0;
+function setMinesNegsCount(cellI, cellJ, board) {
+  for (var i = cellI-1; i <= cellI + 1; i++) {
+    if (i < 0 || i >= board.length) continue;
+    for (var j = cellJ - 1; j <= cellJ + 1; j++) {
+      if (j < 0 || j >= board[i].length) continue;
+      if (i === cellI && j === cellJ) continue;
+      if (board[i][j].cellType === BOMB) minesNegsCount++;
+    }
+  }
+  return minesNegsCount;
 }
