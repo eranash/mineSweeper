dd807c8f861dd2c4b3da5319c1f15ce37767141c
eranash <45369755+eranash@users.noreply.github.com>
2021-10-04 20:50:01 +0300

    v1.2

--- app.js
+++ app.js
@@ -1,58 +1,90 @@
 const grid = document.querySelector(".grid");
 var gBoard;
+var gCellIdx = 0;
+var numOfNegs;
+var gBegginer = {
+    size:4,
+    bombAmount:2
+}
 
-var size = 4;
-var bombAmount = 2;
 const BOMB = "bomb";
 const VALID = "valid";
 
-const bombs = Array(bombAmount).fill(BOMB);
-const valids = Array(size * size - bombAmount).fill(VALID);
+const bombs = Array(gBegginer.bombAmount).fill(BOMB);
+const valids = Array(gBegginer.size * gBegginer.size - gBegginer.bombAmount).fill(VALID);
 const gCells = bombs.concat(valids);
-// console.log(gCells)
-createBoard(size);
-console.log(gBoard);
+
+function init() {
+  // console.log(gCells)
+  createBoard(gBegginer.size);
+  // console.log(gBoard);
+  renderBoard(gBoard);
+  setMinesNegsCount(gBoard);
+}
 
 function createBoard() {
   const board = [];
-  for (var i = 0; i < size; i++) {
+  for (var i = 0; i < gBegginer.size; i++) {
     board.push([]);
-    for (var j = 0; j < size; j++) {
-      setMinesNegsCount(i, j, board);
-      board[i][j] = drawNum();
+    for (var j = 0; j < gBegginer.size; j++) {
+      var cell = {
+        cellIdx: gCellIdx,
+        minesAroundCount: null,
+        isShown: false,
+        cellType: drawNum(),
+        isMarked: true,
+      };
+      board[i][j] = cell;
+      gCellIdx++;
     }
   }
   gBoard = board;
 }
+// setMinesNegsCount(gBoard);
+function setMinesNegsCount(gBoard) {
+  //   var elCell = Document.querySelector("td");
+  for (var i = 0; i < gBoard.length; i++) {
+    for (var j = 0; j < gBoard[i].length; j++) {
+      numOfNegs = countNegs(i, j, gBoard);
+      console.log("numOfNegs", numOfNegs, "i", i, "j", j);
 
-renderBoard(gBoard);
-function renderBoard() {
-  var idx = 0;
-  for (var i = 0; i < size; i++) {
-    var elRow = document.createElement("tr");
-    grid.appendChild(elRow);
-    for (var j = 0; j < size; j++) {
-      var elCell = document.createElement("td");
-      elRow.appendChild(elCell);
-      elCell.setAttribute("id", idx);
-      elCell.classList.add(gBoard[i][j].cellType);
-      idx++;
-      setMinesNegsCount(i, j, gBoard);
-      elCell.setAttribute("data", minesNegsCount);
+      //   elCell.setAttribute("data", numOfNegs);
     }
   }
+
+  return numOfNegs;
 }
-
-
-var minesNegsCount = 0;
-function setMinesNegsCount(cellI, cellJ, board) {
-  for (var i = cellI-1; i <= cellI + 1; i++) {
+console.log(gBoard);
+function countNegs(cellI, cellJ, board) {
+  var minesNegsCount = 0;
+  for (var i = cellI - 1; i <= cellI + 1; i++) {
     if (i < 0 || i >= board.length) continue;
     for (var j = cellJ - 1; j <= cellJ + 1; j++) {
       if (j < 0 || j >= board[i].length) continue;
       if (i === cellI && j === cellJ) continue;
+      // console.log(i,j)
+
       if (board[i][j].cellType === BOMB) minesNegsCount++;
     }
   }
   return minesNegsCount;
+}
+
+function renderBoard() {
+  var idx = 0;
+  for (var i = 0; i < gBegginer.size; i++) {
+    var elRow = document.createElement("tr");
+    grid.appendChild(elRow);
+    for (var j = 0; j < gBegginer.size; j++) {
+      var elCell = document.createElement("td");
+      elRow.appendChild(elCell);
+      elCell.addEventListener("click", function (e) {
+        console.log("click");
+      });
+      elCell.setAttribute("id", idx);
+      elCell.classList.add(gBoard[i][j].cellType);
+
+      idx++;
+    }
+  }
 }
--- util.js
+++ util.js
@@ -1,15 +1,9 @@
 function drawNum() {
-    var idx = getRandomInt(0, gCells.length)
-    var cell = {
-        cellType:gCells[idx],
-        minesAroundCount:4,
-        isShown:true,
-        isMine:false,
-        isMarked:true 
-    }
-    gCells.splice(idx, 1)
-    return cell
-}
+    var idx = getRandomInt(0, gCells.length-1);
+    var num = gCells[idx];
+    gCells.splice(idx, 1);
+    return num;
+  }
 
 
 
@@ -17,4 +11,18 @@ function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function countNeighbors(cellI, cellJ, mat) {
+    var neighborsCount = 0;
+    for (var i = cellI - 1; i <= cellI + 1; i++) {
+        if (i < 0 || i >= mat.length) continue;
+        for (var j = cellJ - 1; j <= cellJ + 1; j++) {
+            if (j < 0 || j >= mat[i].length) continue;
+            if (i === cellI && j === cellJ) continue;
+            if (mat[i][j] === LIFE || mat[i][j] === SUPER_LIFE) neighborsCount++;
+            // if (mat[i][j]) neighborsCount++;
+        }
+    }
+    return neighborsCount;
 }
