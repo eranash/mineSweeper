3fd4e0c4d3ab415660aba67c592c264771977297
eranash <45369755+eranash@users.noreply.github.com>
2021-10-05 13:06:52 +0300

    v1.3

--- app.js
+++ app.js
@@ -2,31 +2,36 @@ const grid = document.querySelector(".grid");
 var gBoard;
 var gCellIdx = 0;
 var numOfNegs;
+var isGameover = false
 var gBegginer = {
-    size:4,
-    bombAmount:2
-}
+  size: 4,
+  bombAmount: 2,
+};
+var checkedCells = {
 
+};
+var size = 4;
+var bombAmount = 2;
 const BOMB = "bomb";
 const VALID = "valid";
 
-const bombs = Array(gBegginer.bombAmount).fill(BOMB);
-const valids = Array(gBegginer.size * gBegginer.size - gBegginer.bombAmount).fill(VALID);
+const bombs = Array(bombAmount).fill(BOMB);
+const valids = Array(size * size - bombAmount).fill(VALID);
 const gCells = bombs.concat(valids);
 
 function init() {
   // console.log(gCells)
-  createBoard(gBegginer.size);
-  // console.log(gBoard);
+  createBoard(size);
+  console.log(gBoard);
   renderBoard(gBoard);
   setMinesNegsCount(gBoard);
 }
 
 function createBoard() {
   const board = [];
-  for (var i = 0; i < gBegginer.size; i++) {
+  for (var i = 0; i < size; i++) {
     board.push([]);
-    for (var j = 0; j < gBegginer.size; j++) {
+    for (var j = 0; j < size; j++) {
       var cell = {
         cellIdx: gCellIdx,
         minesAroundCount: null,
@@ -42,19 +47,22 @@ function createBoard() {
 }
 // setMinesNegsCount(gBoard);
 function setMinesNegsCount(gBoard) {
-  //   var elCell = Document.querySelector("td");
+    var counter = 0;
   for (var i = 0; i < gBoard.length; i++) {
     for (var j = 0; j < gBoard[i].length; j++) {
       numOfNegs = countNegs(i, j, gBoard);
-      console.log("numOfNegs", numOfNegs, "i", i, "j", j);
-
-      //   elCell.setAttribute("data", numOfNegs);
+    //   console.log("numOfNegs", numOfNegs, "i", i, "j", j);
+      var elCell = document.getElementById(counter+'');
+      elCell.setAttribute("data", numOfNegs);
+      counter++
     }
+    
   }
+  
 
   return numOfNegs;
 }
-console.log(gBoard);
+
 function countNegs(cellI, cellJ, board) {
   var minesNegsCount = 0;
   for (var i = cellI - 1; i <= cellI + 1; i++) {
@@ -70,21 +78,67 @@ function countNegs(cellI, cellJ, board) {
   return minesNegsCount;
 }
 
-function renderBoard() {
-  var idx = 0;
-  for (var i = 0; i < gBegginer.size; i++) {
-    var elRow = document.createElement("tr");
-    grid.appendChild(elRow);
-    for (var j = 0; j < gBegginer.size; j++) {
-      var elCell = document.createElement("td");
-      elRow.appendChild(elCell);
-      elCell.addEventListener("click", function (e) {
-        console.log("click");
-      });
-      elCell.setAttribute("id", idx);
-      elCell.classList.add(gBoard[i][j].cellType);
 
-      idx++;
+function renderBoard(board){
+    var idx = 0;
+    var strHtml='';
+
+    for (var i=0; i<size; i++){
+        strHtml+= `<tr>`
+        for (var j=0; j<size; j++){
+            var cell = board[i][j];
+            var className = cell.cellType;
+            // console.log(className);
+            strHtml+= ` <td id="${idx}" class="cell ${className}" onclick="cellClicked(event, this,${i},${j})"></td>`;
+            idx++
+        }
+        
+        strHtml+= `</tr>`
     }
-  }
+    var elBoard = document.querySelector('.board');
+    elBoard.innerHTML=strHtml;
 }
+
+function cellClicked(eve, elCell, i, j){
+    if (isGameover) return
+    var cellId = elCell.getAttribute('id').toString();
+    console.log('elcell', elCell.getAttribute('id'))
+    console.log(checkedCells, i,j)
+    if (checkedCells[cellId]) {return}
+    checkedCells[cellId]=true;
+    console.log(checkedCells, i,j)
+    
+    if (elCell.classList.contains(BOMB)){
+        console.log('game ovaer')
+    } else{
+        var total = elCell.getAttribute('data');
+        console.log(total)
+        if (total !=0){
+            elCell.classList.add('checked')
+            elCell.innerText = total
+            return
+        }
+        checkNegs(i,j,gBoard)
+        elCell.classList.add('checked')
+    }
+}
+
+
+function checkNegs(cellI, cellJ, board) {
+   
+    for (var i = cellI - 1; i <= cellI + 1; i++) {
+      if (i < 0 || i >= board.length) continue;
+      for (var j = cellJ - 1; j <= cellJ + 1; j++) {
+        if (j < 0 || j >= board[i].length) continue;
+        if (i === cellI && j === cellJ) continue;
+        var cellObj= gBoard[i][j];
+        var cellId = cellObj.cellIdx
+        console.log(cellId)
+        var cellElement = document.getElementById(cellId)
+        cellClicked(null,cellElement,i,j)
+  
+        if (board[i][j].cellType === BOMB) minesNegsCount++;
+      }
+    }
+    
+  }
